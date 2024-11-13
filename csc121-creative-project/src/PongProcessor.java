961eefd0ac701457dece7f119d4ae37f588ec845
JacobBridges20 <jacob.bridges@vikings.berry.edu>
2024-11-13 12:05:18 -0500

    Pong Processer

---
+++ csc121-creative-project/src/PongProcessor.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class PongProcessor {
+
+	public static void main(String[] args) {
+
+		System.out.println ("Processing Background"); 
+
+		System.out.println ("Enter File Name"); 
+
+		// get user's input from "standard input" --- ususaly the keyboard
+		Scanner kb = new Scanner (System.in); 
+		String filename = kb.nextLine(); 
+		
+		System.out.println("Reading from" + filename + "..."); 
+	
+ 
+		
+		
+		// open the data file 
+		Scanner sc = new Scanner (new File(filename));
+		
+		// open an output file 
+		PrintWriter pw = new PrintWriter (new File ("output txt")); 
+		
+		
+		
+	}
+
+}
